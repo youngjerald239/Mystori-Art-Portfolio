0637169bc35fefccbb2532da6331a08ba6ae9c9c
youngjerald239 <youngjerald239@gmail.com>
2022-02-13 00:36:05 -0600

    updated layout to fit mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import "../style.css"
 import Layout from "../layout/Layout"
 import {TwitterTweetEmbed} from "react-twitter-embed"
+import Zoom from 'react-reveal/Zoom'
+import Bounce from 'react-reveal/Bounce'
 
 
 export default function Blog() {
@@ -9,7 +11,8 @@ export default function Blog() {
 
   return (
     <Layout>
-      <h1>Mystori Art's Portfolio</h1>
+    <Zoom>
+      <h1 className="port">Welcome To My Portfolio!</h1>
 
       <h2>About Me</h2>
       
@@ -18,10 +21,13 @@ export default function Blog() {
         I created Mystori Art to showcase my interests in character design, fanart, landscapes, and many other digital art projects. I have developed my own style to match my tastes as a black artist, please checkout my ko-fi. Be sure to checkout my most recent works bellow, with timelapses included!
       </p>
       <div className="aboutme">
+      <Bounce bottom>
       <TwitterTweetEmbed className="about" tweetId={"1439625859606921232"}/>
+      </Bounce>
       </div>
+      </Zoom>
       <div className="widgets">
-           
+        <Bounce bottom>   
            <div className="widgets__widgetContainer">
               <div className="tweet">
                <TwitterTweetEmbed className="tweets" tweetId={"1480598757314019330"}/>
@@ -43,6 +49,7 @@ export default function Blog() {
                
                
            </div>
+           </Bounce>
         </div>
         <div className="kofi"><iframe id='kofiframe' src='https://ko-fi.com/mystoriart/?hidefeed=true&widget=true&embed=true&preview=true'  height='400' width='1523' title='mystoriart'></iframe></div>
     </Layout>
